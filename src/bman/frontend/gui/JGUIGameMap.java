191a479a6c954cfdf4c02ed0effe17fd9f41bed0
petol <petter.olsen89@gmail.com>
2013-04-16 16:36:03 +0200

    removed folder

---
+++ src/bman/frontend/gui/JGUIGameMap.java
@@ -0,0 +1,13 @@
+package bman.frontend.gui;
+
+public class JGUIGameMap extends JGUIScreen {
+
+	/**
+	 * VARIABLES
+	 */
+	private static final long serialVersionUID = -5735185698246996895L;
+
+	
+	
+	
+}
---
+++ src/bman/frontend/gui/JGUIMapObject.java
@@ -0,0 +1,6 @@
+package bman.frontend.gui;
+
+public class JGUIMapObject {
+
+
+}
