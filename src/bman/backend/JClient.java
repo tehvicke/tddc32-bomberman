6238575376a7c6a694d1a4a295ebbc630ad6e22d
vikda574 <vikda574@student.liu.se>
2013-05-07 12:13:51 +0200

    Ful.

--- src/bman/backend/JClient.java
+++ src/bman/backend/JClient.java
@@ -32,22 +32,21 @@ public class JClient implements Runnable{
 
 	public void UDPEventHandler(UDPEvent event) {
 		if (event.type == UDPEventInterface.Type.game_start) {
+			
+			/* Positionen */
+			String[] args = {Integer.toString(3), Integer.toString(4)};		
+			client.sendEvent(new UDPEvent(Type.player_join, this.id, args));
+			
 			startGame();
 			
 			
-			String[] args = {Integer.toString(3), Integer.toString(4)};
-			
-			client.sendEvent(new UDPEvent(Type.player_join, this.id, args));
-		
-		
-		
 		}
 		if (event.type == UDPEventInterface.Type.game_end) {
 			//endGame();
 		}
 		if (event.type == UDPEventInterface.Type.player_join) {
 			String[] arg = event.getArguments();
-			addPlayer(id,Integer.parseInt(arg[0]),Integer.parseInt(arg[1]));
+			addPlayer(event.getOriginID(), Integer.parseInt(arg[0]),Integer.parseInt(arg[1]));
 		}
 		if (event.type == UDPEventInterface.Type.player_move_up) {
 			movePlayer(event.getOriginID(), 0, -1);
