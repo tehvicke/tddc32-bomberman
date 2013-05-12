58855b650d72afb40da28432c08f2143a9965585
Viktor Dahl <vikda574@student.liu.se>
2013-05-12 11:22:26 +0200

    isalive works.

--- src/bman/networking/UDPClient.java
+++ src/bman/networking/UDPClient.java
@@ -144,6 +144,7 @@ public class UDPClient implements UDPClientInterface, Runnable {
 			} catch (SocketTimeoutException e) {
 				if (!isAlive) {
 					System.out.println("Server svarar ej!!");
+					break;
 				}
 				this.sendEvent(new UDPEvent(UDPEventInterface.Type.is_alive, this.playerHash));
 				isAlive = false;
